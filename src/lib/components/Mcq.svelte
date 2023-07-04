3d71fac96f831f9471c464a1b27e9be939ebccf8
junitacademy <junit.academy@gmail.com>
2023-07-04 09:26:09 +0530

    add MCQ Component

---
+++ src/lib/components/Mcq.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { MCQ } from '$lib/models/mcq.js';
+
+	export let mcq: MCQ;
+</script>
+
+<div class="multiple-choice-question">
+	<div class="question">
+		{@html mcq.question.content}
+	</div>
+	<div class="options">
+		{#each mcq.options as option}
+			<div on:click={() => console.log(option)} class="option">
+				{@html option.content}
+			</div>
+		{/each}
+	</div>
+	<div class="solution">
+		{@html mcq.solution.content}
+	</div>
+</div>
