bdcdaaab54685154a45aad88cf9b01cf931786a0
junitacademy <junit.academy@gmail.com>
2023-07-04 10:08:17 +0530

    update adpter

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Mcq from '$lib/components/Mcq.svelte';
+	import type { MCQ } from '$lib/models/mcq.js';
+
+	let mcq: MCQ = {
+		question: {
+			content: '<p>This is sample question.</p>'
+		},
+		options: [
+			{
+				content: '<p>Option 1</p>'
+			},
+			{
+				content: '<p>Option 2</p>',
+				isCorrect: true
+			}
+		],
+		solution: {
+			content: '<p>This is Sample Solution.</p>'
+		}
+	};
+</script>
+
+<Mcq {mcq} />
